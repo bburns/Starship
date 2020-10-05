f61ca0087de370b2aacc71438360091c1f33e82c
Brian Burns <bburns.km@gmail.com>
2020-10-05 14:32:12 -0500

    Updates

---
+++ precache-manifest.2a00373f8ac503870a42bb35652a7c75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9c1f2556a41d23b72596ea613dd7ca",
+    "url": "/Starship/index.html"
+  },
+  {
+    "revision": "1cca50798e1c2715819d",
+    "url": "/Starship/static/css/main.e332cb79.chunk.css"
+  },
+  {
+    "revision": "2090b6ebe6131a8537e4",
+    "url": "/Starship/static/js/2.75ed24b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Starship/static/js/2.75ed24b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cca50798e1c2715819d",
+    "url": "/Starship/static/js/main.230f1592.chunk.js"
+  },
+  {
+    "revision": "9d82cd114caa703b6f17d0a00f134def",
+    "url": "/Starship/static/js/main.230f1592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e01422802880b4c464f2",
+    "url": "/Starship/static/js/runtime-main.0c125e01.js"
+  }
+]);
